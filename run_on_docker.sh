1781e1d416028551077f23c30b262fe58d147f21
anatawa12 <anatawa12@icloud.com>
2021-04-26 22:58:43 +0900

    fix: should run with --rm

--- run_on_docker.sh
+++ run_on_docker.sh
@@ -4,5 +4,5 @@ IMAGE_NAME="anatawa12/forgegradle-example-runner:$(git rev-parse --short HEAD)"
 CONTINER_NAME="anatawa12-forgegradle-example-runner-$(date "+%Y%m%d%H%M%S")"
 
 docker build -t "$IMAGE_NAME" --build-arg SETUP_TASK="$1" .
-docker run --name "$CONTINER_NAME" "$IMAGE_NAME"
+docker run --rm --name "$CONTINER_NAME" "$IMAGE_NAME"
 docker rmi "$IMAGE_NAME"
